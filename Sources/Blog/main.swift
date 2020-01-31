2bac83b433b4ae44ffb178e157563b270cc855f1
Botao Li <1114338553@qq.com>
2020-01-30 19:14:42 -0500

    Update splash plugin

--- Sources/Blog/main.swift
+++ Sources/Blog/main.swift
@@ -1,9 +1,11 @@
 import Foundation
 import Publish
 import Plot
+import SplashPublishPlugin
 
 // This will generate your website using the built-in Foundation theme:
 try Blog().publish(
     withTheme: .blog,
-    additionalSteps: [.deploy(using: .gitHub("tonystarkli/blog"))]
+    additionalSteps: [.deploy(using: .gitHub("tonystarkli/blog"))],
+    plugins: [.splash(withClassPrefix: "")]
 )
